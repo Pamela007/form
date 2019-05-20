fc3527f57aea03d5cdd56a50510e35c1048f7789
pamela <pameladjouma89@gmail.com>
2019-05-20 11:47:27 +0200

    ma premiere page web

---
+++ pages/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404
+from pages.models import Article
+from .forms import ContactForm
+
+def contact(request):
+    # Construire le formulaire, soit avec les données postées,
+    # soit vide si l'utilisateur accède pour la première fois
+    # à la page.
+    form = ContactForm(request.POST or None)
+    # Nous vérifions que les données envoyées sont valides
+    # Cette méthode renvoie False s'il n'y a pas de données
+    # dans le formulaire ou qu'il contient des erreurs.
+    if form.is_valid():
+        # Ici nous pouvons traiter les données du formulaire
+        sujet = form.cleaned_data['sujet']
+        message = form.cleaned_data['message']
+        envoyeur = form.cleaned_data['envoyeur']
+        renvoi = form.cleaned_data['renvoi']
+
+        # Nous pourrions ici envoyer l'e-mail grâce aux données
+        # que nous venons de récupérer
+        envoi = True
+
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+    return render(request, 'pages/contact.html', locals())
+
+
+def accueil(request):
+    """ Afficher tous les articles de notre blog """
+    articles = Article.objects.all() # Nous sélectionnons tous nos articles
+    return render(request, 'pages/accueil.html', {'derniers_articles': articles})
+def lire(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'pages/lire.html', {'article':article})
